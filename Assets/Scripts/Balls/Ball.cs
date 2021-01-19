3c1696193d99ecfbf9699f951b4597cee8048048
Bornespb <alex_striker@mail.ru>
2021-01-19 13:30:49 +0300

    Начальный возврат модуля HelixJump

---
+++ Assets/Scripts/Balls/Ball.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out PlatformSegment segment))
+        {
+            other.GetComponentInParent<Platform>().Break();
+        }
+    }
+}
---
+++ Assets/Scripts/Platforms/FinishPlatformSegment.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FinishPlatformSegment : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Ball ball))
+        {
+            Debug.Log("Ball has reached final platform.");
+        }
+    }
+}
